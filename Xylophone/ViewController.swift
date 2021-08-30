12524fe58bff190099be4d4b2d3f9a205d60cbf4
abdel-elsayed <abdel.elsayed18@gmail.com>
2021-08-30 17:23:24 +0200

    Aded delay functionality when button is clicked by 2 seconds

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -17,14 +17,23 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound(sound: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        guard let buttonTitle = sender.currentTitle else{ return }
+            
+            playSound(sound: buttonTitle)
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
+        
     }
     
     func playSound(sound: String) {
         let url = Bundle.main.url(forResource: sound, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
-                
     }
 }
 
