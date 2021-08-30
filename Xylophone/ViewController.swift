97d86e6498454be3bb6b76e3f175d91e5767bcdf
abdel-elsayed <abdel.elsayed18@gmail.com>
2021-08-30 16:54:10 +0200

    added buttons to the IBAction to produce the sounds when pressed.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -5,20 +5,26 @@
 //  Created by Angela Yu on 28/06/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-    @IBAction func keyPressed(_ sender: UIButton) {
-        print("I got pressesd")
-    }
     
+    var player: AVAudioPlayer!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(sound: sender.currentTitle!)
+    }
     
-
+    func playSound(sound: String) {
+        let url = Bundle.main.url(forResource: sound, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
 
